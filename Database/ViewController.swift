4ae7ab3a498bca8bf2784e1697ad9c474b870f58
dviet <dviet@outlook.com>
2016-09-30 14:46:30 +0700

    Initial Commit

---
+++ Database/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Database
+//
+//  Created by Duc Pham Viet on 9/30/16.
+//  Copyright © 2016 Duc Pham Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
